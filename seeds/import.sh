45c40c7c77a7e1c34e81a90dbf0e78041b29a4ee
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-01-22 13:00:26 -0500

    Add _id attributes to all segments
    - Improve JSON import script

--- seeds/import.sh
+++ seeds/import.sh
@@ -2,9 +2,35 @@
 
 # Import JSON content into MongoDB.
 
+function status () {
+    echo "      [$1] $2"
+}
+
+function banner () {
+    echo "====== $1 ======"
+}
+
 for c in courses listings offerings quizzes roles semesters topics users
 do
-    echo ===== ${c} =====
-    mongo --quiet faraday --eval "db.$c.drop()"
-    mongoimport --quiet --db=faraday --collection=${c} --jsonArray --file=${c}.json
+    echo "== $c"
+    output=$(mongo --quiet faraday --eval "db.$c.drop()")
+    if [[ $output = "true" ]]
+    then
+        status "mongo" "dropped $c"
+    else
+        status "mongo" "failed to drop $c; continuing"
+    fi
+
+    output=$(mongoimport --quiet --db=faraday --collection=${c} --jsonArray --file=${c}.json)
+    if [[ $? == 0 ]]
+    then
+        status "mongoimport" "imported $c"
+    else
+        status "mongoimport" "failed to import $c"
+        banner "FAILURE"
+        exit 1
+    fi
 done
+
+banner "COMPLETE"
+exit 0
