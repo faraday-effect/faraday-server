43dd68efd91f851c383edcb59b69d2e0aaf5323b
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-02-24 19:03:19 -0500

    Add endpoint to retrieve the current semester.

--- plugins/semesters.js
+++ plugins/semesters.js
@@ -1,11 +1,20 @@
 // @flow
 
-import Boom from 'boom';
-import Joi from 'joi';
+import moment from "moment";
 
 // CRUD
 async function readAllSemesters(mongo: $FlowTODO) {
     return await mongo.db.collection('semesters').find().toArray();
+}
+
+async function readCurrentSemester(mongo: $FlowTODO) {
+    const now = moment().format('YYYY-MM-DD');
+    return await mongo.db
+        .collection('semesters')
+        .findOne({
+            'courseDates.instruction.start': {$lte: now},
+            'courseDates.instruction.end': {$gte: now}
+        });
 }
 
 // API
@@ -19,6 +28,13 @@ const semestersPlugin = {
                 path: '/api/semesters',
                 handler: async function(request, h) {
                     return await readAllSemesters(request.mongo);
+                }
+            },
+            {
+                method: 'GET',
+                path: '/api/semesters/current',
+                handler: async function (request, h) {
+                    return await readCurrentSemester(request.mongo);
                 }
             }
         ]);
